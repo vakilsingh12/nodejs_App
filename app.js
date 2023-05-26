3aa97d5e14ae0c942666ba0c77181fb903a39944
Vakil.Verma <vakil@nimapinfotech.com>
2023-05-27 02:56:54 +0530

    inital setup done

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 app.use("/api/v1", routes);
-require("./connection/db");
+// require("./connection/db");
 app.listen(port || 6000, () => {
   console.log(`Server is running on port ${port}`);
 });
