6ecd3f23943737d69f314de32c1e1e4c4aeaa73a
Vakil.Verma <vakil@nimapinfotech.com>
2023-05-27 10:20:26 +0530

    inital setup done

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const port = process.env.PORT;
 app.use(express.json());
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
-app.use("/api/v1", routes);
 app.get('/',(req,res)=>{
   return res.status(200).json("Hello World");
 })
-require("./connection/db");
+// app.use("/api/v1", routes);  
+// require("./connection/db");
 app.listen(port || 4000, () => {
   console.log(`Server is running on port ${port}`);
 });
